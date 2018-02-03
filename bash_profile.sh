26b0c3c37e3d8b0a3e312fec85b2614b185dace5
Tor Håvard Bieltvedt <konbietor@gmail.com>
2018-02-03 10:58:09 +0100

    Added JAVA_TOOL_OPTIONS setting to avoid Java Coffee Cup icon appearing in the Dock on Mac OSX.

--- bash_profile.sh
+++ bash_profile.sh
@@ -157,6 +157,10 @@ export PATH=$PATH:$JAVA_HOME/bin:$GIT_HOME/cmd:$MVN_HOME/bin:$SONAR_RUNNER_HOME/
 ### Maven ###
 export MAVEN_OPTS='-Xmx512m -XX:MaxPermSize=256m'
 
+### Java ###
+# apple.awt.UIElement=true will stop Java Coffee Cup icon in the Dock on Mac OSX
+export JAVA_TOOL_OPTIONS="-Dapple.awt.UIElement=true"
+
 
 ### Bash profile ###
 alias    list='cat $SCRIPT_DIR/bash_profile.sh'
